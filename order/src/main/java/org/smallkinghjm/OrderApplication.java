70c0648cc5a91a4730d53bb762390edb4d724dfe
smallkinghjm <2955164425@qq.com>
2022-04-10 13:43:51 +0800

    nacos-config

--- order/src/main/java/org/smallkinghjm/OrderApplication.java
+++ order/src/main/java/org/smallkinghjm/OrderApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @RibbonClients(value = {
@@ -14,6 +17,21 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class OrderApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication.class,args);
+        ConfigurableApplicationContext run = SpringApplication.run(OrderApplication.class, args);
+        printConfig(run);
+    }
+
+    //动态感知nacos-config的配置
+    private static void printConfig(ConfigurableApplicationContext run){
+        while (true){
+            String userName = run.getEnvironment().getProperty("user.name");
+            String password = run.getEnvironment().getProperty("user.password");
+            System.out.println("name:"+userName+";password:"+password);
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- order/src/main/java/org/smallkinghjm/controller/OrderController.java
+++ order/src/main/java/org/smallkinghjm/controller/OrderController.java
@@ -3,6 +3,8 @@ package org.smallkinghjm.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.smallkinghjm.feign.StockFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +12,11 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 @RestController
 @RequestMapping("/order")
+@RefreshScope//支持@Value的动态感知
 public class OrderController {
+
+    @Value("${user.name}")
+    private String name;
 
     @Autowired
     StockFeignService stockFeignService;
@@ -20,5 +26,10 @@ public class OrderController {
         log.info("下单");
         String msg = stockFeignService.demo();
         return msg+"下单成功";
+    }
+
+    @RequestMapping("/config")
+    public String show(){
+        return name;
     }
 }
